d00f12b12443ac466ad3b4edd01bd74e86ef83c5
hasankarim18 <hallysust@gmail.com>
2023-12-06 16:10:15 +0600

    Logical semester adding

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,8 +1,21 @@
-import { TAcademicSemester } from './academicSemester.interface'
+import {
+  TAcademicSemester,
+  TacademicSemesterNameCodeMapper,
+} from './academicSemester.interface'
 import { AcademicSemester } from './academicSemester.model'
 
 // create semester into db
 const createAcademicSemesterIntoDb = async (payload: TAcademicSemester) => {
+  const academicSemesterNameCodeMapper: TacademicSemesterNameCodeMapper = {
+    Autumn: '01',
+    Summer: '02',
+    Fall: '03',
+  }
+  // academicSemesterNameCodeMapper['Fall'] !== '03'
+  if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
+    throw new Error('Invalid Semester Code')
+  }
+
   const result = await AcademicSemester.create(payload)
 
   return result
