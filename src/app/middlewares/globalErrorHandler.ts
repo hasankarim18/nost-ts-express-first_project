30e05903daa75b58e2c8c88d90434764e3944685
hasankarim18 <hallysust@gmail.com>
2023-12-23 12:13:43 +0600

    castError, duplicateError, validationError

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -4,8 +4,12 @@
 
 import { ErrorRequestHandler, NextFunction, Request, Response } from 'express'
 import { ZodError, ZodIssue } from 'zod'
-import { TErrorSouce } from '../interface/error'
+import { TErrorSources } from '../interface/error'
 import config from '../config'
+import { handleZodError } from '../errors/handleZodError'
+import { handleValidationError } from '../errors/handleValidationError'
+import handleCastError from '../errors/handleCastError'
+import handleDuplicateError from '../errors/handleDuplicateError'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
@@ -14,7 +18,7 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let message = err.message || 'Something went wrong!!!'
 
   // array of object
-  let errorSouces: TErrorSouce = [
+  let errorSources: TErrorSources = [
     {
       path: '',
       message: 'Something went wrong!!!',
@@ -22,35 +26,34 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   ]
   //### setting default value ends
 
-  const handleZodError = (err: ZodError) => {
-    const errorSouces: TErrorSouce = err.issues.map((issue: ZodIssue) => {
-      return {
-        path: issue?.path[issue.path.length - 1],
-        message: issue?.message,
-      }
-    })
-    statusCode = 400
-    // message = 'ami zod error'
-
-    return {
-      statusCode,
-      message: 'Common Validation Error.',
-      errorSouces,
-    }
-  }
-
   if (err instanceof ZodError) {
     const simplifiedError = handleZodError(err)
     statusCode = simplifiedError?.statusCode
     message = simplifiedError?.message
-    errorSouces = simplifiedError?.errorSouces
+    errorSources = simplifiedError?.errorSources
     // console.log(simplifiedError)
+  } else if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err)
+    statusCode = simplifiedError?.statusCode
+    message = simplifiedError?.message
+    errorSources = simplifiedError?.errorSources
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err)
+    statusCode = simplifiedError?.statusCode
+    message = simplifiedError?.message
+    errorSources = simplifiedError?.errorSources
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err)
+    statusCode = simplifiedError?.statusCode
+    message = simplifiedError?.message
+    errorSources = simplifiedError?.errorSources
   }
 
   return res.status(statusCode).json({
     success: false,
     message: message,
-    errorSouces,
+    errorSouces: errorSources,
+    err,
     stack: config.NODE_ENV === 'development' ? err?.stack : null,
   })
   // next()
